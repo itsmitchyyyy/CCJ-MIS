f9beba5d84430e76dab8f8804d0be6af398b9c37
Mitch Alforque <mitch@vananaz.com>
2024-04-21 16:19:49 +0800

    Remove unused logo and title elements from Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,5 @@
 import { HeaderWrapper, LogoWrapper, TitleWrapper } from './elements';
 import logo from '@/assets/images/logo.png';
-import anotherLogo from '@/assets/images/logo_2.png';
 
 export type HeaderProps = {};
 
@@ -10,14 +9,6 @@ export const Header = () => {
       <TitleWrapper>
         <LogoWrapper>
           <img src={logo} alt="logo" />
-        </LogoWrapper>
-      </TitleWrapper>
-      <TitleWrapper>
-        <h1>ERMS</h1>
-      </TitleWrapper>
-      <TitleWrapper>
-        <LogoWrapper className="anotherLogo">
-          <img src={anotherLogo} alt="logo" />
         </LogoWrapper>
       </TitleWrapper>
     </HeaderWrapper>
